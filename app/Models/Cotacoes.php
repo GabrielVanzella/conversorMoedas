fdd7b19e728d421c3939300e47f7b0964741dc3e
Gabriel Vanzella <gabrielvanzella1@hotmail.com>
2022-03-29 12:55:29 -0300

    php artisan make:model Cotacoes -m

---
+++ app/Models/Cotacoes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cotacoes extends Model
+{
+    use HasFactory;
+}
